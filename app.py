10f7b8fe95bf24a1b8c0e6108f5535db3bfd716b
Stefany Mojica <stefymojica40@gmail.com>
2023-10-05 10:19:59 -0500

    adding label - independient variables

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 
 model = ('../prediction/model.pkl')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='Prediction Heart Failure')
+    html.Div(children='Prediction Heart Failure'),
+
+    html.Br(),
+    html.Label('Age'),
+    dcc.Input(value='Anote age', type='number'),
+
+    html.Br(),
+    html.Label('Anaemia'),
+    dcc.Input(value='Anote anaemia', type='number'),
+
+    html.Br(),
+    html.Label('Diabetes'),
+    dcc.Input(value='Anote Diabetes', type='number'),
+
+    html.Br(),
+    html.Label('Ejection fraction'),
+    dcc.Input(value='Anote Ejection fraction', type='number'),
+
+    html.Br(),
+    html.Label('High blood pressure'),
+    dcc.Input(value='Anote High blood pressure', type='number'),
+
+    html.Br(),
+    html.Label('Serum creatinine'),
+    dcc.Input(value='Anote Serum creatinine', type='number'),
+
+    html.Br(),
+    html.Label('Sex'),
+    dcc.Input(value='Anote Sex', type='number'),
+
+    html.Br(),
+    html.Label('Smoking'),
+    dcc.Input(value='Anote Smoking', type='number'),
 ])
 
 app.run(debug=True, port=8060)
