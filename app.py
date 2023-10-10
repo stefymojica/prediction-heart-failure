d40a9c6764c949db2eb3d2b3ab96d601017f0fd2
Stefany Mojica <stefymojica40@gmail.com>
2023-10-10 12:11:17 -0500

    setting predictions

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from dash import Dash, html, dcc, callback, Output, Input
+import pickle
+from sklearn.neural_network import MLPClassifier
 
-model = ('../prediction/model.pkl')
+model = 'src/prediction/model.pkl'
+with open(model, 'rb') as file:
+    model = pickle.load(file)
 
 app = Dash(__name__)
 
@@ -43,35 +47,35 @@ app.layout = html.Div([
 
                     html.Br(),
                     html.Label('Anaemia'),
-                    dcc.Dropdown(id='my-input-2',
-                                 options=['O', '1'],
-                                 value='Anote anaemia',
-                                 style={
-                                     'textAlign': 'center',
-                                     'margin-bottom': '10px',
-                                     'width': '160px'}
-                                 ),
-                    # dcc.Input(id='my-input-2',value='Anote Anaemia', type='number',
-                    #         style={
-                    #             'textAlign': 'center',
-                    #             'margin-bottom': '10px',
-                    #             'width': '150px'}),
+                    # dcc.Dropdown(id='my-input-2',
+                    #              options=['O', '1'],
+                    #              value='Anote anaemia',
+                    #              style={
+                    #                  'textAlign': 'center',
+                    #                  'margin-bottom': '10px',
+                    #                  'width': '160px'}
+                    #              ),
+                    dcc.Input(id='my-input-2', value='Anote Anaemia', type='number',
+                              style={
+                                  'textAlign': 'center',
+                                  'margin-bottom': '10px',
+                                  'width': '150px'}),
 
                     html.Br(),
                     html.Label('Diabetes'),
-                    dcc.Dropdown(id='my-input-3',
-                                 options=['O', '1'],
-                                 value='Anote Diabetes',
-                                 style={
-                                     'textAlign': 'center',
-                                     'margin-bottom': '10px',
-                                     'width': '160px'}
-                                 ),
-                    # dcc.Input(id='my-input-3',value='Anote Diabetes', type='number',
-                    #         style={
-                    #             'textAlign': 'center',
-                    #             'margin-bottom': '10px',
-                    #             'width': '150px'}),
+                    # dcc.Dropdown(id='my-input-3',
+                    #              options=['O', '1'],
+                    #              value='Anote Diabetes',
+                    #              style={
+                    #                  'textAlign': 'center',
+                    #                  'margin-bottom': '10px',
+                    #                  'width': '160px'}
+                    #              ),
+                    dcc.Input(id='my-input-3', value='Anote Diabetes', type='number',
+                              style={
+                                  'textAlign': 'center',
+                                  'margin-bottom': '10px',
+                                  'width': '150px'}),
 
                     html.Br(),
                     html.Label('Ejection fraction'),
@@ -92,19 +96,19 @@ app.layout = html.Div([
                 children=[
                     html.Br(),
                     html.Label('High blood pressure'),
-                    dcc.Dropdown(id='my-input-5',
-                                 options=['O', '1'],
-                                 value='Anote High blood pressure',
-                                 style={
-                                     'textAlign': 'center',
-                                     'margin-bottom': '10px',
-                                     'width': '160px'}
-                                 ),
-                    # dcc.Input(id='my-input-5',value='Anote High blood pressure', type='number',
-                    #         style={
-                    #             'textAlign': 'center',
-                    #             'margin-bottom': '10px',
-                    #             'width': '150px'}),
+                    # dcc.Dropdown(id='my-input-5',
+                    #              options=['O', '1'],
+                    #              value='Anote High blood pressure',
+                    #              style={
+                    #                  'textAlign': 'center',
+                    #                  'margin-bottom': '10px',
+                    #                  'width': '160px'}
+                    #              ),
+                    dcc.Input(id='my-input-5', value='Anote High blood pressure', type='number',
+                              style={
+                                  'textAlign': 'center',
+                                  'margin-bottom': '10px',
+                                  'width': '150px'}),
 
                     html.Br(),
                     html.Label('Serum creatinine'),
@@ -115,35 +119,35 @@ app.layout = html.Div([
                                   'width': '150px'}),
                     html.Br(),
                     html.Label('Sex'),
-                    dcc.Dropdown(id='my-input-7',
-                                 options=['O', '1'],
-                                 value='Sex',
-                                 style={
-                                     'textAlign': 'center',
-                                     'margin-bottom': '10px',
-                                     'width': '160px'}
-                                 ),
-                    # dcc.Input(id='my-input-7',value='Anote Sex', type='number',
-                    #         style={
-                    #             'textAlign': 'center',
-                    #             'margin-bottom': '10px',
-                    #             'width': '150px'}),
+                    # dcc.Dropdown(id='my-input-7',
+                    #              options=['O', '1'],
+                    #              value='Sex',
+                    #              style={
+                    #                  'textAlign': 'center',
+                    #                  'margin-bottom': '10px',
+                    #                  'width': '160px'}
+                    #              ),
+                    dcc.Input(id='my-input-7', value='Anote Sex', type='number',
+                              style={
+                                  'textAlign': 'center',
+                                  'margin-bottom': '10px',
+                                  'width': '150px'}),
 
                     html.Br(),
                     html.Label('Smoking'),
-                    dcc.Dropdown(id='my-input-8',
-                                 options=['O', '1'],
-                                 value='Smoking',
-                                 style={
-                                     'textAlign': 'center',
-                                     'margin-bottom': '10px',
-                                     'width': '160px'}
-                                 ),
-                    # dcc.Input(id='my-input-8',value='Anote Smoking', type='number',
-                    #         style={
-                    #             'textAlign': 'center',
-                    #             'margin-bottom': '10px',
-                    #             'width': '150px'}),
+                    # dcc.Dropdown(id='my-input-8',
+                    #              options=['O', '1'],
+                    #              value='Smoking',
+                    #              style={
+                    #                  'textAlign': 'center',
+                    #                  'margin-bottom': '10px',
+                    #                  'width': '160px'}
+                    #              ),
+                    dcc.Input(id='my-input-8', value='Anote Smoking', type='number',
+                              style={
+                                  'textAlign': 'center',
+                                  'margin-bottom': '10px',
+                                  'width': '150px'}),
                 ])
         ]),
     html.Div(
@@ -152,7 +156,31 @@ app.layout = html.Div([
             'margin-top': '70px'
         }, children=[
             html.Button('Click here to see the result', id='show-secret'),
+            html.Div(id='resultado')
         ])
 ])
+
+
+@callback(
+    Output('resultado', 'children'),
+    Input('show-secret', 'n_clicks'),
+    [Input('my-input-1', 'value'), Input('my-input-2', 'value'),
+     Input('my-input-3', 'value'), Input('my-input-4', 'value'),
+     Input('my-input-4', 'value'), Input('my-input-5', 'value'),
+     Input('my-input-7', 'value'), Input('my-input-8', 'value')]
+)
+def do_prediction(n_clicks, value1, value2, value3, value4, value5, value6, value7, value8):
+    if n_clicks is None:
+        return None
+    predicts = model.predict(
+        [[value1, value2, value3, value4, value5, value6, value7, value8]])
+
+    if predicts[0] == 0:
+        return "Negativo"
+
+    else:
+        return "Positivo"
+
+
 if __name__ == '__main__':
     app.run(debug=True,  port=8080)
